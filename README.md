e68c87b586fb584caf04517d6b4fe0c9ff486dd5
Dalton Tan <daltonyi@hotmail.com>
2021-02-15 22:49:20 +0800

    init

---
+++ README.md
@@ -0,0 +1,76 @@
+# PostCSS Spacing
+
+Generate spacing utilities classes with margins and paddings. The classes are named using the format
+`{property}{sides}-{size}`.
+
+Where property is one of:
+
+- `m` - for classes that set `margin`
+- `p` - for classes that set `padding`
+
+Where sides is one of:
+
+- `t` - for classes that set `margin-top` or `padding-top`
+- `b` - for classes that set `margin-bottom` or `padding-bottom`
+- `l` - for classes that set `margin-left` or `padding-left`
+- `r` - for classes that set `margin-right` or `padding-right`
+- `x` - for classes that set both `*-left` and `*-right`
+- `y` - for classes that set both `*-top` and `*-bottom`
+- blank - for classes that set a `margin` or `padding` on all 4 sides of the element
+
+The size increases exponentially:
+
+- `auto` - for classes that set the `margin` to `auto`
+- `0` - for classes that eliminate the `margin` or `padding` by setting it to `0`
+- `1` - for classes that set the `margin` or `padding` to unit * `0.25`
+- `2` - for classes that set the `margin` or `padding` to unit * `0.5`
+- `3` - for classes that set the `margin` or `padding` to unit * `1`
+- `4` - for classes that set the `margin` or `padding` to unit * `2`
+
+And so on.
+
+You can also use negative margins by prending `n` to the size such as `ml-n2`.
+
+## Usage
+
+Install plugin.
+
+```
+npm i -D postcss-spacing
+```
+
+Add to you PostCSS config:
+
+```
+module.exports = {
+	plugins: [
+		require('postcss-spacing'),
+	]
+}
+```
+
+Add to your CSS file:
+
+```
+@generate-spacing;
+```
+
+## Options
+
+### unit
+
+Default: 0.25
+
+The base unit that will be used to calculate sizes for every subsequent increase in spacing. 
+
+### start
+
+Default: 0
+
+The unit size to start from. Set to negative number to generate negative margins.
+
+### end
+
+Default: 4
+
+The highest unit size to generate. Increasing this will generate more classes with exponentially increasing size.
---
+++ index.js
@@ -0,0 +1,98 @@
+module.exports = (opts = {}) => {
+	return {
+		postcssPlugin: 'postcss-spacing',
+		Once(root, api) {
+			root.walkAtRules(atRule => {
+				if (atRule.name === 'generate-spacing') {
+					spacingPlugin(opts, atRule, api)
+				}
+			})
+		},
+		// cssnano will delete it if we use AtRule
+		// AtRule: {
+		// 	'generate-spacing': (atRule, api) => {
+		// 		spacingPlugin(opts, atRule, api)
+		// 	}
+		// }
+	}
+}
+module.exports.postcss = true
+
+function spacingPlugin(opts, placeholder, { Rule, Declaration, Comment }) {
+	const unit = opts.unit || 0.25
+	const directions = {
+		// placed in order so that they can overwrite correctly
+		t: ['top'],
+		r: ['right'],
+		b: ['bottom'],
+		l: ['left'],
+		x: ['left', 'right'],
+		y: ['top', 'bottom'],
+		'': [],
+	}
+
+	function multiply(n) {
+		let result = n ? (n < 0 ? -unit : unit) : 0
+		n = Math.abs(n)
+		for (let i = 1; i < n; i++) {
+			result *= 2
+		}
+		return result
+	}
+
+	function generate(type, alias, direction, props, multiplier) {
+		const postfix = multiplier < 0 ? 'n' + Math.abs(multiplier) : multiplier
+		const rule = new Rule({ selector: `.${alias}${direction}-${postfix}` })
+		if (direction === '') {
+			rule.append(new Declaration({
+				prop: type,
+				value: multiply(multiplier) + 'rem'
+			}))
+		} else {
+			props.map((cur) => {
+				rule.append(new Declaration({
+					prop: `${type}-${cur}`,
+					value: multiply(multiplier) + 'rem'
+				}))
+			})
+		}
+		placeholder.after(rule)
+	}
+
+	const start = opts.start || 0
+	const end = opts.end || 4
+	for (let dir in directions) {
+		// can start from negative to allow negative margins
+		for (let i = start; i <= end; i++) {
+			generate('margin', 'm', dir, directions[dir], i)
+			if (i >= 0) {
+				generate('padding', 'p', dir, directions[dir], i)
+			}
+		}
+	}
+	placeholder.after(new Rule({ selector: `.mr-a` }).append(new Declaration({
+		prop: 'margin-right',
+		value: 'auto'
+	}), new Declaration({
+		prop: 'display',
+		value: 'block'
+	})))
+	placeholder.after(new Rule({ selector: `.ml-a` }).append(new Declaration({
+		prop: 'margin-left',
+		value: 'auto'
+	}), new Declaration({
+		prop: 'display',
+		value: 'block'
+	})))
+	placeholder.after(new Rule({ selector: `.mx-a` }).append(new Declaration({
+		prop: 'margin-left',
+		value: 'auto'
+	}), new Declaration({
+		prop: 'margin-right',
+		value: 'auto'
+	}), new Declaration({
+		prop: 'display',
+		value: 'block'
+	})))
+	placeholder.remove()
+}
